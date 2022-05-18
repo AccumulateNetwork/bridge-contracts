ee63eb799250db9e34678f7223144b868c395bfc
ilzheev <a@lovedesign.ru>
2022-05-18 09:46:22 +0200

    events indexes added

--- contracts/AccumulateBridge.sol
+++ contracts/AccumulateBridge.sol
@@ -7,10 +7,10 @@ contract AccumulateBridge is Ownable {
 
     constructor() {}
 
-    event LogTransferTokenOwnership(WrappedToken _token, address _newOwner);
-    event LogRenounceTokenOwnership(WrappedToken _token);
-    event LogMint(WrappedToken token, address to, uint256 amount);
-    event LogBurn(WrappedToken token, string destination, uint256 amount);
+    event LogTransferTokenOwnership(WrappedToken indexed _token, address indexed _newOwner);
+    event LogRenounceTokenOwnership(WrappedToken indexed _token);
+    event LogMint(WrappedToken indexed token, address indexed to, uint256 amount);
+    event LogBurn(WrappedToken indexed token, string indexed destination, uint256 amount);
 
     function transferTokenOwnership(WrappedToken token, address newOwner) public onlyOwner {
         token.transferOwnership(newOwner);
